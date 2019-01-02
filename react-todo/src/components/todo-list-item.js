a741ab018d17d658fc102673a23e4fde5e17b0ab
Andrey Krylov <dante_040591@mail.ru>
2019-01-02 13:21:05 +0300

    work in attributes on React components

--- react-todo/src/components/todo-list-item.js
+++ react-todo/src/components/todo-list-item.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const TodoListItem = () => {
-    return <span>Wake Up</span>
+const TodoListItem = ( props ) => {
+
+    const styleItem = {
+        color: props.important ? 'red' : 'black'
+    };
+
+    return <span style={styleItem}>{props.label}</span>
 };
 
 export default TodoListItem;
+
+//OR
+
+// const TodoListItem = ( {label, important = false} ) => {
+
+//     const styleItem = {
+//         color: important ? 'red' : 'black'
+//     };
+
+//     return <span style={styleItem}>{label}</span>
+// };
+
+// export default TodoListItem;
